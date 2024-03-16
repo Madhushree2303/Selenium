6de6c7c6f5ee5d540038f5d5e5cc83e047a8ca0c
Madhushree2303 <madhu.balan023@gmail.com>
2024-03-16 23:40:34 +0530

    new code

--- src/test/java/com/testingselenium/Test01.java
+++ src/test/java/com/testingselenium/Test01.java
@@ -1,2 +1,15 @@
-package com.testingselenium;public class Test01 {
+package com.testingselenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test01 {
+    @Test
+    public void openGoogle(){
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+    }
 }
