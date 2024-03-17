159cbd5a789c147e9ccc74ca1102b8e8606929d3
Madhushree2303 <madhu.balan023@gmail.com>
2024-03-17 22:44:46 +0530

    new testcases added

---
+++ src/test/java/com/testingselenium/Testcase05_Vmomaximizescreen.java
@@ -0,0 +1,26 @@
+package com.testingselenium.Testcase05_app;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class Vmomaximizescreen {
+
+    @Test
+    public void maximizeAppVmo(){
+
+        ChromeOptions chromeOptions=new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+
+        WebDriver driver;
+        driver=new ChromeDriver(chromeOptions);
+        driver.get("https://google.com");
+
+        driver.quit();
+        driver=new EdgeDriver();
+        driver.get("https://www.saucedemo.com/inventory.html");
+
+    }
+}
---
+++ src/test/java/com/testingselenium/Testcase06_vmoAppNavigateMethod.java
@@ -0,0 +1,2 @@
+package com.testingselenium;public class Testcase06_vmoAppNavigateMethod {
+}
